547d7957b5d5ea630bfb9471efd4f2287326da55
just_nik <nikitayuvchenko@gmail.com>
2026-03-21 14:52:28 +0300

    Made a client class in hn_client.py , connected fastapi and made a simple endpoint

--- hn_client.py
+++ hn_client.py
@@ -5,16 +5,21 @@ from typing import Any
 
 from models import Story
 
-async def get_topstories_id(count: int = 10) -> list[int]:
-    try:
-        if not isinstance(count, int):
-            raise TypeError(f"Expected type is 'int' not {type(count)}")
-        if count <=0:
-            raise ValueError("Expected count which is > 0")
-        
-        async with AsyncClient() as cli:
-            response = await cli.get("https://hacker-news.firebaseio.com/v0/topstories.json", timeout=10)
+
+class HNClient:
+    
+    def __init__(self, client: AsyncClient):
+        self.client = client
+
+    async def get_topstories_id(self, count: int = 10) -> list[int]:
+        try:
+            if not isinstance(count, int):
+                raise TypeError(f"Expected type is 'int' not {type(count)}")
+            if count <=0:
+                raise ValueError("Expected count which is > 0")
             
+            response = await self.client.get("https://hacker-news.firebaseio.com/v0/topstories.json", timeout=10)
+                
             response.raise_for_status()
             content_type = response.headers.get("Content-Type", "")
 
@@ -23,63 +28,65 @@ async def get_topstories_id(count: int = 10) -> list[int]:
 
             data = response.json()
 
-        if not isinstance(data, list) :
-            raise TypeError("Expected list from API")
+            if not isinstance(data, list) :
+                raise TypeError("Expected list from API")
+            
+            return data[:count]
         
-        return data[:count]
+        except httpx_exc.RequestError:
+            raise
+
+    async def _fetch_story_json(self, id: int) -> dict[str, Any]:
+        try:
+            if not isinstance(id, int):
+                raise TypeError(f"Expected type is 'int' not {type(id)}")
+            
+            response = await self.client.get(f"https://hacker-news.firebaseio.com/v0/item/{id}.json", timeout=10)
+            response.raise_for_status()
+            content_type = response.headers.get("Content-Type", "")
+
+            if "application/json" not in content_type:
+                raise TypeError("Unexpected content type")
+            
+            data = response.json()
+
+            if not isinstance(data, dict):
+                raise TypeError("Expected dict from API")
+            
+            return data
+
+        except httpx_exc.RequestError:
+            raise
+
+    async def get_story_by_id(self, id: int) -> Story | None:
+        data = await self._fetch_story_json(id)
+        return self._parse_story(data)        
     
-    except httpx_exc.RequestError:
-        raise
-
-async def _fetch_story_json(client: AsyncClient, id: int) -> dict[str, Any]:
-    try:
-        if not isinstance(id, int):
-            raise TypeError(f"Expected type is 'int' not {type(id)}")
-        
-        response = await client.get(f"https://hacker-news.firebaseio.com/v0/item/{id}.json", timeout=10)
-        response.raise_for_status()
-        content_type = response.headers.get("Content-Type", "")
-
-        if "application/json" not in content_type:
-            raise TypeError("Unexpected content type")
-        
-        data = response.json()
-
-        if not isinstance(data, dict):
-            raise TypeError("Expected dict from API")
-        
-        return data
-
-    except httpx_exc.RequestError:
-        raise
-
-
-def _parse_story(data: dict[str, Any]) -> Story | None:
-    if data.get("type", "") != "story":
+    @staticmethod
+    def _parse_story(data: dict[str, Any]) -> Story | None:
+        if data.get("type", "") != "story":
+                return None
+            
+        story_id = data.get("id")
+        if not isinstance(story_id, int):
+            return None
+            
+        title = data.get("title")
+        if not isinstance(title, str):
+            return None
+            
+        author = data.get("by")
+        if not isinstance(author, str):
+            return None
+            
+        url = data.get("url")
+        if not isinstance(url, str):
             return None
         
-    story_id = data.get("id")
-    if not isinstance(story_id, int):
-        return None
+        score = data.get("score")
+        if not isinstance(score, int):
+            return None
         
-    title = data.get("title")
-    if not isinstance(title, str):
-        return None
-        
-    author = data.get("by")
-    if not isinstance(author, str):
-        return None
-        
-    url = data.get("url")
-    if not isinstance(url, str):
-        return None
-    
-    score = data.get("score")
-    if not isinstance(score, int):
-        return None
+        return Story(story_id, title, author, url, score)
 
-    return Story(story_id, title, author, url, score)
 
-async def get_story_by_id(client: AsyncClient, id: int) -> Story | None:
-    data = await _fetch_story_json(client, id)
-    return _parse_story(data)        
--- main.py
+++ main.py
@@ -1,33 +1,28 @@
-import hn_client
-import asyncio
+from fastapi import FastAPI
 
-from pprint import pprint
-
+from  hn_client import HNClient
 from httpx import AsyncClient
 
-from models import Story
+from contextlib import asynccontextmanager
 
-async def get_top_story_ids(ids:list[int])->list[Story | None]:  
-    
-    async with AsyncClient() as cli:
-        tasks = [
-            asyncio.create_task(hn_client.get_story_by_id(cli, story_id))
-            for story_id in ids
-        ]   
+@asynccontextmanager 
+async def lifespan(app: FastAPI):
+    app.state.hn_cli = HNClient(AsyncClient())
+    try:
+        yield
+    finally:
+        await app.state.hn_cli.client.aclose()
         
-        return await asyncio.gather(*tasks)
         
-async def loop():
-    ids = await hn_client.get_topstories_id(5)
-    
-    stories  = await get_top_story_ids(ids)
-    
-    pprint(stories)
-    
+app = FastAPI(lifespan=lifespan)
 
-
-if __name__ == "__main__":
-    asyncio.run(loop())
+@app.get("/stories")
+async def get_stories(count: int = 10) -> list[int]:
+    cli = app.state.hn_cli
+    return await cli.get_topstories_id(count)
+    
+    
+    
     
 
 
